6e242513d6e7b60f8017ade68e8271e9a8d09d00
Yasmeen700 <b7Xy8e5XUZPABAAIrO31nUQPcCnWDIoB>
2020-11-05 11:29:53 +0200

    message

--- lib/main.dart
+++ lib/main.dart
@@ -18,55 +18,62 @@ class MyApp extends StatelessWidget {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Container(
-                  padding: EdgeInsets.all(20),
-                  child: Row(
-                    children: [
-                      Icon(
-                        Icons.account_balance_rounded,
-                        size: 40,
-                      ),
-                      SizedBox(
-                        width: 70,
-                      ),
-                      Text(
-                        DateFormat.yMMMd().format(DateTime.now()),
-                        style: TextStyle(
-                          color: Colors.grey,
-                          fontFamily: 'Roboto',
-                          fontSize: 20,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
+                // Container(
+                //   padding: EdgeInsets.all(20),
+                //   child: Row(
+                //     children: [
+                //       Icon(
+                //         Icons.account_balance_rounded,
+                //         size: 40,
+                //       ),
+                //       SizedBox(
+                //         width: 70,
+                //       ),
+                //       Text(
+                //         DateFormat.yMMMd().format(DateTime.now()),
+                //         style: TextStyle(
+                //           color: Colors.grey,
+                //           fontFamily: 'Roboto',
+                //           fontSize: 20,
+                //           fontWeight: FontWeight.w500,
+                //         ),
+                //       ),
+                //     ],
+                //   ),
+                // ),
                 SizedBox(
-                  height: 50,
+                  height: 20,
                 ),
                 Container(
-                  // decoration: BoxDecoration(
-                  //   color: Colors.transparent,
-                  //   borderRadius: BorderRadius.all(
-                  //     Radius.circular(20),
-                  //   ),
-                  // ),
-                  height: 150,
-                  child: Swiper(
+                  decoration: BoxDecoration(
+                    // color:  Color(0xfff2f2f2),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(20),
+                    ),
+                  ),
+                  height: 200,
+                  child:Swiper(
                     itemBuilder: (BuildContext context, int index) {
-                      return Card(
-
-                        // borderOnForeground: true,
-                        child: new Image.network(
-                          'https://images.unsplash.com/photo-1547721064-da6cfb341d50?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
-                          fit: BoxFit.fill,
+                      return Container(
+                        decoration: BoxDecoration(
+                          color: Color(0xfff2f2f2),
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(20),
+                          ),
+                          image: DecorationImage(
+                            image: NetworkImage(
+                                'https://cdn.vox-cdn.com/thumbor/altkKN7BnaLUpbAVPi6j4xmC2Ug=/0x146:2040x1214/fit-in/1200x630/cdn.vox-cdn.com/uploads/chorus_asset/file/13546110/acastro_181126_1777_bitcoin_0001.jpg'),
+                            fit: BoxFit.fill,
+                          ),
                         ),
                       );
                     },
                     itemCount: 10,
-                    itemWidth: 300.0,
-                    layout: SwiperLayout.STACK,
-                  ),
+                    viewportFraction: 0.8,
+                    scale: 0.9,
+                  )
+
+
                   // child: ListView.builder(
                   //   scrollDirection: Axis.horizontal,
                   //   shrinkWrap: true,
@@ -88,18 +95,18 @@ class MyApp extends StatelessWidget {
                   //   },
                   // ),
                 ),
-                SizedBox(height: 50),
+                SizedBox(height: 10),
                 Container(
                   height: 600,
                   decoration: BoxDecoration(
                     color: Color(0xfff2f2f2),
                     borderRadius: BorderRadius.all(
-                      Radius.circular(20),
+                      Radius.circular(15),
                     ),
                   ),
 
                   // margin: EdgeInsets.all(20),
-                  padding: EdgeInsets.all(20),
+                  padding: EdgeInsets.all(15),
                   child: ListView.builder(
                     scrollDirection: Axis.vertical,
                     // physics: ,
@@ -118,7 +125,7 @@ class MyApp extends StatelessWidget {
                                 Radius.circular(5),
                               ),
                               image: DecorationImage(
-                                  image: AssetImage('assets/images/sky.jpg'),
+                                  image:NetworkImage('https://techcrunch.com/wp-content/uploads/2020/07/GettyImages-887657568.jpg?w=600'),
                                   fit: BoxFit.cover),
                             ),
                           ),
@@ -144,3 +151,24 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+// Swiper(
+// itemBuilder: (BuildContext context, int index) {
+// return Container(
+// decoration: BoxDecoration(
+// color: Color(0xfff2f2f2),
+// borderRadius: BorderRadius.all(
+// Radius.circular(20),
+// ),
+// image: DecorationImage(
+// image: NetworkImage(
+// 'https://cdn.vox-cdn.com/thumbor/altkKN7BnaLUpbAVPi6j4xmC2Ug=/0x146:2040x1214/fit-in/1200x630/cdn.vox-cdn.com/uploads/chorus_asset/file/13546110/acastro_181126_1777_bitcoin_0001.jpg'),
+// fit: BoxFit.fill,
+// ),
+// ),
+// );
+// },
+// itemCount: 10,
+// itemWidth: 300.0,
+// layout: SwiperLayout.STACK,
+// ),
