570e46353cf4618ba3b70cfe5438391111cc6d00
Yasmeen700 <b7Xy8e5XUZPABAAIrO31nUQPcCnWDIoB>
2020-11-05 13:28:30 +0200

    message

--- lib/controller/applicationcontroller.dart
+++ lib/controller/applicationcontroller.dart
@@ -1,6 +1,6 @@
-import 'package:get/get.dart';
-import '../controller/applicationcontroller.dart';
-
-class ApplicationController extends GetxController{
-
-}
+// import 'package:get/get.dart';
+// import '../controller/applicationcontroller.dart';
+//
+// class ApplicationController extends GetxController{
+//
+// }
--- lib/main.dart
+++ lib/main.dart
@@ -13,38 +13,67 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: SafeArea(
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                // Container(
-                //   padding: EdgeInsets.all(20),
-                //   child: Row(
-                //     children: [
-                //       Icon(
-                //         Icons.account_balance_rounded,
-                //         size: 40,
-                //       ),
-                //       SizedBox(
-                //         width: 70,
-                //       ),
-                //       Text(
-                //         DateFormat.yMMMd().format(DateTime.now()),
-                //         style: TextStyle(
-                //           color: Colors.grey,
-                //           fontFamily: 'Roboto',
-                //           fontSize: 20,
-                //           fontWeight: FontWeight.w500,
-                //         ),
-                //       ),
-                //     ],
-                //   ),
-                // ),
-                SizedBox(
-                  height: 20,
+        appBar: AppBar(
+          leading: Icon(Icons.menu_rounded,
+          color: Colors.black,
+          ),
+          elevation: 5,
+          backgroundColor: Colors.white,
+          centerTitle: true,
+          title: Text(
+            'News',
+            style: TextStyle(
+              color: Colors.black,
+              fontFamily: 'Roboto' ,
+              fontWeight: FontWeight.w600,
+              fontSize: 20,
+            ),
+          ),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // Container(
+              //   padding: EdgeInsets.all(20),
+              //   child: Row(
+              //     children: [
+              //       Icon(
+              //         Icons.account_balance_rounded,
+              //         size: 40,
+              //       ),
+              //       SizedBox(
+              //         width: 70,
+              //       ),
+              //       Text(
+              //         DateFormat.yMMMd().format(DateTime.now()),
+              //         style: TextStyle(
+              //           color: Colors.grey,
+              //           fontFamily: 'Roboto',
+              //           fontSize: 20,
+              //           fontWeight: FontWeight.w500,
+              //         ),
+              //       ),
+              //     ],
+              //   ),
+              // ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(8, 10,0,0),
+                child: Text('Top news',
+                  style: TextStyle(
+                    fontFamily: 'Roboto' ,
+                    fontSize: 20 ,
+                    fontWeight: FontWeight.w300,
+
+
+                  ),
                 ),
-                Container(
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
                   decoration: BoxDecoration(
                     // color:  Color(0xfff2f2f2),
                     borderRadius: BorderRadius.all(
@@ -52,27 +81,52 @@ class MyApp extends StatelessWidget {
                     ),
                   ),
                   height: 200,
-                  child:Swiper(
+                  child: Swiper(
                     itemBuilder: (BuildContext context, int index) {
-                      return Container(
-                        decoration: BoxDecoration(
-                          color: Color(0xfff2f2f2),
-                          borderRadius: BorderRadius.all(
-                            Radius.circular(20),
+                      return Stack(
+                        children: [
+                          Container(
+                            decoration: BoxDecoration(
+                              color: Color(0xfff2f2f2),
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(20),
+                              ),
+                              image: DecorationImage(
+                                image: NetworkImage(
+                                    'https://cdn.arstechnica.net/wp-content/uploads/2017/08/GettyImages-510365916-760x380.jpg'),
+                                fit: BoxFit.fill,
+                              ),
+                            ),
                           ),
-                          image: DecorationImage(
-                            image: NetworkImage(
-                                'https://cdn.vox-cdn.com/thumbor/altkKN7BnaLUpbAVPi6j4xmC2Ug=/0x146:2040x1214/fit-in/1200x630/cdn.vox-cdn.com/uploads/chorus_asset/file/13546110/acastro_181126_1777_bitcoin_0001.jpg'),
-                            fit: BoxFit.fill,
-                          ),
-                        ),
+                          Container(
+                              alignment: Alignment.bottomLeft,
+                              child:    Padding(
+                                padding: const EdgeInsets.fromLTRB(8, 10,0,10),
+                                child: ListTile(
+                                  title: Text('Latest news',
+                                    style: TextStyle(
+                                      fontFamily: 'Roboto' ,
+                                      fontSize: 20 ,
+                                      fontWeight: FontWeight.w500,
+                                      color: Colors.white
+                                    ),
+                                  ),
+                                  subtitle: Text('Latest news',
+                                    style: TextStyle(
+                                        fontFamily: 'Roboto' ,
+                                        // fontSize: 20 ,
+                                        color: Colors.white70,
+                                    ),
+                                  ),
+                                ),
+                              ),),
+                        ],
                       );
                     },
                     itemCount: 10,
                     viewportFraction: 0.8,
                     scale: 0.9,
                   )
-
 
                   // child: ListView.builder(
                   //   scrollDirection: Axis.horizontal,
@@ -94,81 +148,74 @@ class MyApp extends StatelessWidget {
                   //     );
                   //   },
                   // ),
-                ),
-                SizedBox(height: 10),
-                Container(
-                  height: 600,
-                  decoration: BoxDecoration(
-                    color: Color(0xfff2f2f2),
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(15),
-                    ),
                   ),
+              SizedBox(height: 10),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(8, 10,0,10),
+                child: Text('Latest news',
+                  style: TextStyle(
+                    fontFamily: 'Roboto' ,
+                    fontSize: 20 ,
+                    fontWeight: FontWeight.w300,
 
-                  // margin: EdgeInsets.all(20),
-                  padding: EdgeInsets.all(15),
-                  child: ListView.builder(
-                    scrollDirection: Axis.vertical,
-                    // physics: ,
-                    shrinkWrap: true,
-                    itemCount: 8,
-                    itemBuilder: (BuildContext context, int index) {
-                      return Card(
-                        child: ListTile(
-                          onTap: () {
-                            //  TODO: add GetX to navigate
-                          },
-                          leading: Container(
-                            width: 70,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.all(
-                                Radius.circular(5),
-                              ),
-                              image: DecorationImage(
-                                  image:NetworkImage('https://techcrunch.com/wp-content/uploads/2020/07/GettyImages-887657568.jpg?w=600'),
-                                  fit: BoxFit.cover),
-                            ),
-                          ),
-                          title: Text(
-                            'title',
-                            style: TextStyle(
-                              fontSize: 20,
-                              fontFamily: "Roboto",
-                              fontWeight: FontWeight.w600,
-                            ),
-                          ),
-                          subtitle: Text('subtitle'),
-                        ),
-                      );
-                    },
+
                   ),
                 ),
-              ],
-            ),
+              ),
+              Container(
+                height: 600,
+                decoration: BoxDecoration(
+                  color: Color(0xfff2f2f2),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(15),
+                  ),
+                ),
+
+                // margin: EdgeInsets.all(20),
+                padding: EdgeInsets.all(15),
+                child: ListView.builder(
+                  scrollDirection: Axis.vertical,
+                  // physics: ,
+                  shrinkWrap: true,
+                  itemCount: 8,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Card(
+                      child: ListTile(
+                        onTap: () {
+                          //  TODO: add GetX to navigate
+                        },
+                        leading: Container(
+                          width: 70,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(5),
+                            ),
+                            image: DecorationImage(
+                                image: NetworkImage(
+                                    'https://techcrunch.com/wp-content/uploads/2020/07/GettyImages-887657568.jpg?w=600'),
+                                fit: BoxFit.cover),
+                          ),
+                        ),
+                        title: Text(
+                          'title',
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontFamily: "Roboto",
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        subtitle: Text('subtitle',
+                    style: TextStyle(
+                    fontFamily: 'Roboto' ,),
+                    ))
+                    );
+                  },
+                ),
+              ),
+            ],
           ),
         ),
       ),
     );
   }
 }
-
-// Swiper(
-// itemBuilder: (BuildContext context, int index) {
-// return Container(
-// decoration: BoxDecoration(
-// color: Color(0xfff2f2f2),
-// borderRadius: BorderRadius.all(
-// Radius.circular(20),
-// ),
-// image: DecorationImage(
-// image: NetworkImage(
-// 'https://cdn.vox-cdn.com/thumbor/altkKN7BnaLUpbAVPi6j4xmC2Ug=/0x146:2040x1214/fit-in/1200x630/cdn.vox-cdn.com/uploads/chorus_asset/file/13546110/acastro_181126_1777_bitcoin_0001.jpg'),
-// fit: BoxFit.fill,
-// ),
-// ),
-// );
-// },
-// itemCount: 10,
-// itemWidth: 300.0,
-// layout: SwiperLayout.STACK,
-// ),
